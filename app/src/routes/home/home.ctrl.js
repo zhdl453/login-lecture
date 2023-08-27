bc7205d648040755e2ea210f7f25e53fd7f5c57b
Haribo Lee(이혜영) <130369365+zhdl453@users.noreply.github.com>
2023-08-27 18:27:32 +0000

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,43 @@
 //이전으로 되감아서 컨트롤러 기능을 갖는  '콜백 함수'를 복사하려는 것입니다.
 //hello,login라는 컨트롤러 함수를 만들고, 이를 외부에서 사용해준다
 "use strict";
-function hello (req, res) {
-   res.render("home/index");
+
+const output = {
+    home: (req, res) => {
+       res.render("home/index");
+    },
+    
+    login: (req, res)=> {
+        res.render("home/login");
+    }
 };
 
-function login(req, res) {
-    res.render("home/login");
-}
+const users = {
+    id: ["zhdl453", "id004", "id005"],
+    psword: ["dud4533025", "pw004", "pw005"]
+};
 
+const process = {
+    login: (req,res)=>{
+        const id = req.body.id,
+        psword = req.body.psword;//이 body값을 잘 파싱해줄수 있도록 모듈을 설치해줘야함
+        //이 req는 리퀘스트 용청 데이터들을 담아두는 변수이다.
+        //src/public/js/home login.js에서 body: JSON.stringify(req) <= req 변수에 담아준다고 명시해둠
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] ===psword){
+                return res.json({
+                    success: true
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "login has failed"
+        });
+    },
+};
 module.exports = {
-    hello,
-    login,
+    output,
+    process
 };  
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,8 +9,9 @@ const ctrl = require('./home.ctrl'); //저기 경로로 가게 외부모듈로 �
 //app.get()을 -> router.get()로만 바꿔주면 되는데, 이렇게 하면 여기 index.js파일에서만 쓸수 있다.
 //그래서 app.js파일에서 app.use('/', home); 해줘가지고 미들웨어 등록하는 메서드 써서 연결해줘야함
 
-router.get("/", ctrl.hello);
-router.get("/login", ctrl.login);
+router.get("/", ctrl.output.home);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login); //처리한다는 의미의 process로 이름 지어줄거임.(이건 맘대로 바꿔도 됨. 변수이름이니까ㅇㅇ)
 //hello,login => 이전으로 되감아서 컨트롤러 기능을 갖는  '콜백 함수'를 복사하려는 것입니다.
 
 module.exports = router;
