b1df54958d48a670f8a40cb45330835673fb0131
Haribo Lee(이혜영) <130369365+zhdl453@users.noreply.github.com>
2023-08-28 09:27:10 +0000

    userStorage 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,10 +1,29 @@
 "use strict";
+
+const { reduce } = require("async");
+
 //클래스로 만들고, 보통 파일명이랑 같게 해줌. 유저 정도 여기 클래서에 담아줄거인
-class UserStorage {
-    users = {
+class UserStorage { //users를 static으로 전역변수로 만들어주면 클래스 UserStorage가 접근가능함. home.ctrl.js파일에서
+    //const userStorage = new UserStorage();
+    //console.log(UserStorage.users); 이렇게 해줄 필요가 없어짐
+   static #users = { //#은닉화 해주는거: private하게 만들어줌
         id: ["zhdl453", "id004", "id005"],
-        psword: ["dud4533025", "pw004", "pw005"]
+        psword: ["dud4533025", "pw004", "pw005"],
+        name: ["이혜영", "사길동", "오길동"]
     };
+
+   static getUsers(...fields){ //#users처럼 은닉화 해주면 home.ctrl.js파일에서 UserStorage.#users를 못들고옴
+    //그래서 이렇게 getUsers 메소드 만들어줘서 UserStorage.getUsers로 해줘야함
+    //전역변수로 들고왔으니 static getUsers로 맞춰준다 static #users ㅇㅇ
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field)=>{ //newUsers변수에는 fields의 초깃값이 들어가고, field변수에는 나머지값들이 들어감
+        if(users.hasOwnProperty(field)){
+            newUsers[field] = users[field];
+        }
+        return uewUsers;
+    }, {});
+    return newUsers;
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,32 +6,35 @@ const UserStorage = require("../../models/UserStorage");
 
 const output = {
     home: (req, res) => {
-       res.render("home/index");
+       res.render("index");
     },
     
     login: (req, res)=> {
-        res.render("home/login");
+        res.render("login");
     }
 };
 
 const process = {
     login: (req,res)=>{
         const id = req.body.id,
-        psword = req.body.psword;//이 body값을 잘 파싱해줄수 있도록 모듈을 설치해줘야함
+        psword = req.body.psword;
+    
+    console.log(UserStorage.getUsers("id", "psword"));
+        //이 body값을 잘 파싱해줄수 있도록 모듈을 설치해줘야함
         //이 req는 리퀘스트 용청 데이터들을 담아두는 변수이다.
         //src/public/js/home login.js에서 body: JSON.stringify(req) <= req 변수에 담아준다고 명시해둠
         
-        // const response = {};
-        // if(users.id.includes(id)) {
-        //     const idx = users.id.indexOf(id);
-        //     if(users.psword[idx] ===psword){
-        //         response.success = true;
-        //         return res.json(response );
-        //     }
-        // }
-        // response.success =false;
-        // response.msg = "login has failed"
-        // return res.json(response);
+        const response = {};
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] ===psword){
+                response.success = true;
+                return res.json(response);
+            }
+        }
+        response.success =false;
+        response.msg = "login has failed"
+        return res.json(response);
     },
 };
 module.exports = {
