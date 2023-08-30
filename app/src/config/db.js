812272f15b83f9b27eac98bd3b07afc9070f9435
Haribo Lee(이혜영) <130369365+zhdl453@users.noreply.github.com>
2023-08-30 09:19:40 +0000

    db 연결

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host: 'localhost',
     user:'root',
+    port: 3306,
     password:'admin123',
     database:'mirrorCity_rhythm'
 });
