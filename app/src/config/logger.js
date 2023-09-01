ccb48b467ef9bff271ec76eba5189bc2aaecef1c
Haribo Lee(이혜영) <130369365+zhdl453@users.noreply.github.com>
2023-09-01 08:43:34 +0000

    winston 최적화

--- app/src/config/logger.js
+++ app/src/config/logger.js
@@ -7,19 +7,33 @@ const printLogFormat = combine(
     label({
         label: "백엔드 맛보기"
     }),
-  colorize(),
+  //colorize(), new transports.File로 파일 저장할땐 이 컬러라이즈 없애야 오류안뜸
   timestamp({
     format: "YYYY-MM-DD HH:mm:dd",
   }),
   printFormat
 );
-const logger = createLogger({
-  transports: [
-    new transports.File({
-      level: "error",
+
+const opts = {
+    file: new transports.File({
+        filename:"access.log",
+        dirname: "./logs", //이렇게 경로 설정해주면 따로 filename:./logs/access.log쓸 필요없음
+      level: "info",
       format: printLogFormat,
     }),
+}
+
+const logger = createLogger({
+  transports: [
+   
   ],
 }); //transports라는 키값에 배열을 넣어줌.
+
+if(process.env.NODE_ENV !=="production"){ //production: 실제 서비스 중인 서버라는뜻
+    logger.add(new transports.Console({ //logger.add()로 추가해주는거임.
+        level:"info",
+      format: printLogFormat,
+    }))
+};
 
 module.exports = logger;
